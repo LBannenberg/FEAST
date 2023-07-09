a427b60a4ba8f1bb20c77722473715231bf031a4
Lau Bannenberg <lau.bannenberg@gmail.com>
2023-07-09 17:40:52 +0200

    Make recipe easier to read

--- experiment_01.py
+++ experiment_01.py
@@ -7,12 +7,25 @@ EXPERIMENT_NAME = common.parameters['OUTPUT_DIR'] + 'experiment_01'
 ALGORITHM_NAME = 'original_two-rate'
 
 sentence = '|'.join([
-    'numeric_ternary:if|boolean_binary_num:<=',
-    'numeric_nullary_random:uniform',
-    'numeric_ternary:if|boolean_nullary_observable:best_child_is_low|numeric_nullary:0.75|numeric_nullary:0.25',
-    'numeric_binary:max', 'numeric_binary:/', 'numeric_nullary_observable:rate', 'numeric_nullary:2', 'numeric_nullary:2',
-    'numeric_binary:min', 'numeric_binary:*', 'numeric_nullary_observable:rate', 'numeric_nullary:2', 'numeric_binary:/',
-    'numeric_nullary_observable:dimension', 'numeric_nullary:4',
+    'numeric_ternary:if',
+        'boolean_binary_num:<=',  # rand() <= s
+            'numeric_nullary_random:uniform',
+            'numeric_ternary:if',  # s = 3/4 if best_child_is_low else 1/4
+                'boolean_nullary_observable:best_child_is_low',
+                    'numeric_nullary:0.75',
+                    'numeric_nullary:0.25',
+        'numeric_binary:max',  # max(rate/2, 2)
+            'numeric_binary:/',
+                'numeric_nullary_observable:rate',
+                'numeric_nullary:2',
+            'numeric_nullary:2',
+        'numeric_binary:min',  # min(rate*2, dimension/4)
+            'numeric_binary:*',
+                'numeric_nullary_observable:rate',
+                'numeric_nullary:2',
+            'numeric_binary:/',
+                'numeric_nullary_observable:dimension',
+                'numeric_nullary:4',
 ])
 root = tree.create(sentence)
 
